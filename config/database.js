77cacc73b16cc745954a9724e2a1e6db3a8a23b8
Bhavuk Mittal <vvbnmittal@gmail.com>
2024-12-29 14:25:32 +0530

    configed prod db url

--- config/database.js
+++ config/database.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: "postgres",
-    host: process.env.DATABASE_HOST,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  host: process.env.DATABASE_HOST,
+  dialectOptions: {},
+});
 
 module.exports = sequelize;
