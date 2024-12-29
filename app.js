0d04c127902a3583683eae23c092b86acded376a
Bhavuk Mittal <vvbnmittal@gmail.com>
2024-12-29 16:10:10 +0530

    added cors

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const seatRoutes = require("./routers/seat");
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://train-seat-reservation-demo.netlify.app",
+];
 app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 app.use("/api/user", userRoutes);
